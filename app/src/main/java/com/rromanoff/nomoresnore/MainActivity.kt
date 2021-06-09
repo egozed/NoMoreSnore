820127787771bd460a7d096beaf14fbc090cd7ec
egozed <ego-z@mail.ru>
2021-06-09 23:36:56 +0500

    little refact

--- app/src/main/java/com/rromanoff/nomoresnore/MainActivity.kt
+++ app/src/main/java/com/rromanoff/nomoresnore/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_NOSENSOR
         ui=Ui(this)
         ui.create() //создаем визуалку
         permissions=Permissions(this)
